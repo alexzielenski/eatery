c30cdfa15e48a9c2d51f43f9d6c5a733cf03b5a1
Alexander Zielenski <alex@alexzielenski.com>
2014-11-07 00:32:18 -0500

    sorted friends list w/ priminary group view

--- Eatery/Eatery/FriendsViewController.swift
+++ Eatery/Eatery/FriendsViewController.swift
@@ -8,13 +8,99 @@
 
 import UIKit
 
-class FriendsViewController: UIViewController {
+private func primaryLetterForUser(user: User) -> String {
+    // Typically return first letter of last name, but if
+    // that isnt available, first letter of name
+    let primaryName = user.lastName != nil && user.lastName.length > 0 ? user.lastName : user.name
+    return primaryName.substringToIndex(1)
+}
 
+private var FRIENDSCTX = 0
+class FriendsViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+    private(set) var sortedFriends = NSDictionary()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.tableView.allowsSelection = false
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "FriendCell")
+        self.tableView.registerClass(GroupsTableViewCell.self, forCellReuseIdentifier: "GroupsCell")
+        User.sharedInstance.addObserver(self, forKeyPath: "friendsList", options: NSKeyValueObservingOptions.allZeros, context: &FRIENDSCTX)
         view.backgroundColor = UIColor.whiteColor()
+    }
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if (context == &FRIENDSCTX) {
+            self.sortFiendsList(User.sharedInstance.friendsList)
+        } else {
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+        }
+    }
+    
+    private func sortFiendsList(friends: NSArray) {
+        var sorted = NSMutableDictionary()
+        for object in friends {
+            let primary = primaryLetterForUser(object as User)
+            var list: NSMutableArray!
+            if ((sorted.allKeys as NSArray).containsObject(primary)) {
+                list = sorted[primary] as NSMutableArray
+            } else {
+                list = NSMutableArray()
+                sorted[primary] = list
+            }
+            
+            list.addObject(object)
+        }
+        self.sortedFriends = sorted
         
+        self.tableView.reloadData()
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1 + self.sortedFriends.allKeys.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (section == 0) {
+            return 1
+        }
+        let key: String = self.sortedFriends.allKeys[section - 1] as String
+        return self.sortedFriends[key]!.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if (indexPath.row == 0 && indexPath.section == 0) {
+            let cell: GroupsTableViewCell = tableView.dequeueReusableCellWithIdentifier("GroupsCell", forIndexPath: indexPath) as GroupsTableViewCell
+            return cell
+        }
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("FriendCell", forIndexPath: indexPath) as UITableViewCell
+        let key: String = self.sortedFriends.allKeys[indexPath.section - 1] as String
+        let user:User = self.sortedFriends[key]![indexPath.row]! as User
+        cell.textLabel.text = user.name
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (section == 0) {
+            return nil;
+        }
+        
+        return self.sortedFriends.allKeys[section - 1] as? String
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return indexPath.row == 0 && indexPath.section == 0 ? 72 : tableView.rowHeight
+    }
+    
+    override func sectionIndexTitlesForTableView(tableView: UITableView) -> [AnyObject]! {
+        return self.sortedFriends.allKeys
+    }
+    
+    override func tableView(tableView: UITableView, sectionForSectionIndexTitle title: String, atIndex index: Int) -> Int {
+        return index + 1
+    }
+    
+    deinit {
+        User.sharedInstance.removeObserver(self, forKeyPath: "friendsList", context: &FRIENDSCTX)
     }
 
 }
---
+++ Eatery/Eatery/src/views/GroupsCollectionViewController.swift
@@ -0,0 +1,104 @@
+//
+//  GroupsCollectionViewController.swift
+//  Eatery
+//
+//  Created by Alexander Zielenski on 11/6/14.
+//  Copyright (c) 2014 CUAppDev. All rights reserved.
+//
+
+import UIKit
+
+let reuseIdentifier = "Cell"
+
+class GroupsCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+        self.collectionView.backgroundColor = UIColor.greenColor()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 1
+    }
+
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return 80
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+    
+        // Configure the cell
+        cell.backgroundColor = indexPath.row % 2 == 0 ? UIColor.blackColor() : UIColor.blueColor()
+        
+        println(cell.frame)
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+    
+}
