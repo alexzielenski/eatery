a22f089558cae0ddc7e9997a4d95c081a6dab5ff
Eric Appel <ema77@cornell.edu>
2014-11-09 16:43:03 -0500

    idk

--- Eatery/Eatery/User.swift
+++ Eatery/Eatery/User.swift
@@ -199,8 +199,11 @@ class User: NSObject {
     }
     
     var isLoggedIn: Bool {
-        if let user = PFUser.currentUser() {
-            return true
+        if let user = PFUser.currentUser() { // Check parse
+            if PFFacebookUtils.session().isOpen { // Check facebook
+                return true
+            }
+            return false
         }
         return false
     }
