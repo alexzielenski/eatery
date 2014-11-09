81c5ff6480c9368c29f8604a9e4570b4db9203d6
Manuela Rios <yorios9312@gmail.com>
2014-11-09 16:03:43 -0500

    Added Eat Now Table View
    Custom table view cells, and a push to an empty detail view

--- Eatery/Eatery/EatNowTableViewCell.swift
+++ Eatery/Eatery/EatNowTableViewCell.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class EatNowTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var eateryImage: UIImageView!
+    @IBOutlet weak var eateryName: UILabel!
+    @IBOutlet weak var eateryDesc: UILabel!
+    @IBOutlet weak var eateryMiles: UILabel!
+    @IBOutlet weak var eateryHours: UILabel!
+    
+    func loadItem(#image: String, name: String, desc: String, miles: String, hours: String) {
+        eateryImage.image = UIImage(named: image)
+        eateryImage.layer.cornerRadius = eateryImage.frame.size.width / 2;
+        eateryImage.clipsToBounds = true;
+        eateryName.text = name
+        eateryDesc.text = desc
+        eateryMiles.text = miles
+        eateryHours.text = hours
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
