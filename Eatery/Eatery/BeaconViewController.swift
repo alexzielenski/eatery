5b1fb8a66f96174228ba70c5c2e4373af1846857
Eric Appel <ema77@cornell.edu>
2014-12-08 14:34:20 -0500

    Revert "Develop"

--- Eatery/Eatery/BeaconViewController.swift
+++ Eatery/Eatery/BeaconViewController.swift
@@ -19,7 +19,7 @@ class BeaconViewController: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool) {
-        if !User.isLoggedIn {
+        if !User.sharedInstance.isLoggedIn {
             let signIn = SignInViewController(nibName: "SignInViewController", bundle: nil)
             signIn.title = self.title
             signIn.navigationItem.setHidesBackButton(true, animated: false)
@@ -28,7 +28,6 @@ class BeaconViewController: UIViewController {
                     error!.handleFacebookError()
                 } else {
                     signIn.navigationController?.setViewControllers([self], animated: false)
-                    self.navigationController?.navigationBarHidden = false
                 }
             }
             self.navigationController?.setViewControllers([signIn], animated: false)
